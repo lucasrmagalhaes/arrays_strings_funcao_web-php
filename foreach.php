231eefea6ef0f1c46c3c1465e9b8aa1095cbc9e1
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-10-06 15:11:15 -0300

    Foreach

---
+++ foreach.php
@@ -0,0 +1,20 @@
+<?php
+
+$contasCorrentes = [
+    123 => [
+        'titular' => 'Lucas', 
+        'saldo' => 1000
+    ],
+    456 => [
+        'titular' => 'Maria', 
+        'saldo' => 10000
+    ],
+    789 => [
+        'titular' => 'Roberto', 
+        'saldo' => 300
+    ]
+];
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    echo $cpf . ' - ' . $conta['titular'] . PHP_EOL;
+}
