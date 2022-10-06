a63fbddb0cae8024111e9f99aa27dd77aa0741a9
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-10-06 15:16:56 -0300

    Adicionando dados

--- listas.php
+++ listas.php
@@ -1,7 +1,8 @@
 <?php
 
 $idadeList = [10, 12, 14, 16];
+$idadeList[] = 18;
 
-$primeiraIdade = $idadeList[0];
-
-echo $primeiraIdade;
+foreach ($idadeList as $idade) {
+    echo $idade . PHP_EOL;
+}
