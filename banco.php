8b370082dadf15fffafa39e3585411c7c2afc93e
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-10-06 15:51:43 -0300

    Depositando um valor

--- banco.php
+++ banco.php
@@ -1,15 +1,26 @@
 <?php
 
-function exibeMensagem($mensagem) {
+function exibeMensagem(string $mensagem): void 
+{
     echo $mensagem . PHP_EOL;
 }
 
-function sacar($conta, $valorASacar) {
+function sacar(array $conta, float $valorASacar): array 
+{
     if ($valorASacar > $conta['saldo']) {
         exibeMensagem("Você não pode sacar!");
     } else {
         $conta['saldo'] -= $valorASacar;
     }
+
+    return $conta;
+}
+
+function depositar(array $conta, float $valorADepositar): array
+{
+    $valorADepositar > 0 
+        ? $conta['saldo'] += $valorADepositar 
+        : exibeMensagem("Informe um valor positivo!");
 
     return $conta;
 }
@@ -29,8 +40,10 @@ $contasCorrentes = [
     ]
 ];
 
-$contasCorrentes['123'] = sacar($contasCorrentes['123'], 500);
-$contasCorrentes['789'] = sacar($contasCorrentes['789'], 500);
+// $contasCorrentes['123'] = sacar($contasCorrentes['123'], 500);
+// $contasCorrentes['789'] = sacar($contasCorrentes['789'], 500);
+
+$contasCorrentes['123'] = depositar($contasCorrentes['123'], 500);
 
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem($cpf . ' - ' . $conta['titular'] . ' ' . $conta['saldo']);
