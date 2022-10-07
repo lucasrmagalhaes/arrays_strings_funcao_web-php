6b1220809b648fde0717bcf1a96a27c107c0c544
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-10-07 10:56:48 -0300

    HTML e PHP

--- banco.php
+++ banco.php
@@ -25,8 +25,34 @@ unset($contasCorrentes['456']);
 
 titularComLetrasMaisculas($contasCorrentes['123']);
 
-echo "<ul>";
-    foreach ($contasCorrentes as $cpf => $conta) {
-        exibeConta($conta);
-    }
-echo "</ul>";
+?>
+
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    
+    <body>
+        <h1>Contas correntes</h1>
+
+        <dl>
+            <?php foreach ($contasCorrentes as $cpf => $conta) { ?>
+                <dt>
+                    <h3>
+                        <?= $conta['titular']; ?> - <?= $cpf; ?>
+                    </h3>
+                </dt>
+
+                <dd>
+                    Saldo: <?= $conta['saldo']; ?>
+                </dd>
+            <?php } ?>
+        </dl>
+    </body>
+</html>
